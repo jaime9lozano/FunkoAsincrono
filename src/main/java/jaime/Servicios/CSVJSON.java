59e9b0e36ed9481b75da85d3f5fb5631ec1d5aef
jaime9lozano <jaime9lozano@gmail.com>
2023-10-13 20:05:01 +0200

    V0.3.5

--- src/main/java/jaime/Servicios/CSVJSON.java
+++ src/main/java/jaime/Servicios/CSVJSON.java
@@ -15,7 +15,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
-import java.util.stream.Stream;
 
 public class CSVJSON {
     MyIDStore m=MyIDStore.getInstance();
