1afefc362ccf734b3e642d0ecf029366414e9ef2
jaime9lozano <jaime9lozano@gmail.com>
2023-10-13 14:07:10 +0200

    V0.1

---
+++ src/main/java/jaime/Main.java
@@ -0,0 +1,8 @@
+package jaime;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+
+
+}
